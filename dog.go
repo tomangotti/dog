4704b52ee29513dc12096f97fa35e6c76f3fb1b8
thomas <tom.angotti11@gmail.com>
2023-07-21 11:04:25 -0600

    second

---
+++ dog.go
@@ -0,0 +1,7 @@
+package dog
+
+import "strings"
+
+func WhenGrownUps(s string) string {
+	return "When the puppy grows up it sats: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tomangotti/dog
+
+go 1.20
